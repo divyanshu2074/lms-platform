99e69ca1c8d7ed60dfac8af763b009e4b086264e
divyanshu2074 <divyanshu2074@gmail.com>
2025-12-20 08:05:55 +0530

    feat: seperated the AppContext and AppContextProvider for solving auto-refresh warning

---
+++ client/src/context/AppContextProvider.jsx
@@ -0,0 +1,9 @@
+import { AppContext } from "./AppContext";
+
+const AppContextProvider = ({ children }) => {
+  const value = {};
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export default AppContextProvider;
