d62c298d8cff415d77ae7331b85df2050e76dd0e
divyanshu2074 <divyanshu2074@gmail.com>
2025-12-22 08:48:09 +0530

    feat: created all the Routes for Student and Educator and Outlet in the Educator

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Educator = () => {
-  return <div>Educator</div>;
+  return (
+    <div>
+      Educator
+      <div>{<Outlet />}</div>
+    </div>
+  );
 };
 
 export default Educator;
